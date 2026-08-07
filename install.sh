c70a119e8ae10aab77a1ecbd5c662615a15f3cad
lmh <dev@hilberg.eu>
2026-08-07 16:22:23 +0200

    update flatpak list

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+# Ruft das eigentliche Setup-Skript aus dem Repository auf
+bash <(curl -sL https://raw.githubusercontent.com/tna76874/ansible-silverblue/main/setup.sh)
