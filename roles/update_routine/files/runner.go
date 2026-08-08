92f2d602465e3f8ea03de311e14408c91c934bbb
lmh <dev@hilberg.eu>
2026-08-08 19:08:55 +0200

    update runner

--- roles/update_routine/files/runner.go
+++ roles/update_routine/files/runner.go
@@ -11,8 +11,9 @@ func main() {
 	syscall.Setuid(0)
 	syscall.Setgid(0)
 
-	// Führt deinen gewünschten Befehl aus
+	// Führt den Befehl aus und zwingt HOME auf /root für Pip/Ansible
 	cmd := exec.Command("/bin/bash", "-c", "curl -L https://raw.githubusercontent.com/tna76874/ansible-silverblue/main/setup.sh | bash")
+	cmd.Env = append(os.Environ(), "HOME=/root")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
