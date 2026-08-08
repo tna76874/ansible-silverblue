859c6b3d7b8ca086e784dfa197329ebf83f581d3
lmh <dev@hilberg.eu>
2026-08-08 19:22:12 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -69,8 +69,8 @@ prepare_environment() {
     source venv/bin/activate
 
     echo "Installiere notwendige Werkzeuge im Hintergrund..."
-    pip install --upgrade pip --quiet
-    pip install ansible-core --quiet
+    sudo HOME=/root pip install --upgrade pip --quiet
+    sudo HOME=/root pip install ansible-core --quiet
 
     print_step "Lade Software-Bausteine herunter..."
     ansible-galaxy collection install community.general --force
